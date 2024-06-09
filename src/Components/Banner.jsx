d253d362adcf75fd51ef7e8cb87bb9a0af74720a
Shahriar <shahriar3809@gmail.com>
2024-06-10 01:28:48 +0600

    Lorem remove

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -5,13 +5,13 @@ const Banner = () => {
     <div className="lg:h-[750px] w-full bg-[linear-gradient(to_top,rgba(00,00,00,0.2),rgba(00,00,00,0.5)),url('https://i.ibb.co/2KM2ZyM/rsz-1pexels-vlada-karpovich-4050302.jpg')] text-white bg-cover flex justify-center items-center p-5 ">
       <div className=" md:p-10 w-4/6 md:space-y-12 text-center m-auto">
         <h1 className="md:text-6xl text-3xl md:leading-loose  font-bold">
-          Online Course Can You Purchase or Sell
+          Online Session Purchase or Selling Platform
         </h1>
         <p className="text-xl">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam dolores
-          eum unde ullam qui praesentium quia vero, odio quo ad!
+          Learn to create a platform for buying and selling sessions online,
+          covering essential features like user registration, session listings,
+          secure payments, and user reviews.
         </p>
-        
       </div>
     </div>
   );
